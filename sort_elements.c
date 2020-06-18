67fbef88b0f265d92ab86976def1b0c749583bbd
sudhanshu2005270 <67102533+sudhanshu2005270@users.noreply.github.com>
2020-06-18 16:37:01 +0530

    Create sort_elements.c

---
+++ sort_elements.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,j,opn;
+    printf("Enter the number of elements of array: \n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of the array: \n");
+    for(i=0;i<n;i++)
+        scanf("%d",arr+i);
+    printf("The entered array is: \n");
+    for(i=0;i<n;i++)
+        printf("%d\t",*(arr+i));
+    printf("\n");
+    printf("Enter the choice for sorting: 1. Ascending Order 2. Descending Order\n");
+    scanf("%d",&opn);
+    if(opn==1)
+    {
+        int temp;
+        for(i=0;i<n;i++)
+            for(j=0;j<n-1;j++)
+            {
+                if((arr+j)>(arr+j+1))
+                {
+                    temp=(arr+j);
+                    (arr+j)=(arr+j+1);
+                    (arr+j+1)=temp;
+                }
+            }
+        printf("The sorted array is: \n");
+        for(i=0;i<n;i++)
+            printf("%d\t",*(arr+i));
+    }
+    return 0;
+}
