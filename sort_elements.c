f9d0e5b88d8a2f8eacff83f204ec5eff7efc8f97
sudhanshu2005270 <67102533+sudhanshu2005270@users.noreply.github.com>
2020-06-18 16:38:47 +0530

    Update sort_elements.c

--- sort_elements.c
+++ sort_elements.c
@@ -32,5 +32,25 @@ int main()
         for(i=0;i<n;i++)
             printf("%d\t",*(arr+i));
     }
+    else if(opn==2)
+    {
+        int temp;
+        for(i=0;i<n;i++)
+            for(j=0;j<n-1;j++)
+            {
+                if((arr+j)<(arr+j+1))
+                {
+                    temp=(arr+j);
+                    (arr+j)=(arr+j+1);
+                    (arr+j+1)=temp;
+                }
+            }
+        printf("The sorted array is: \n");
+        for(i=0;i<n;i++)
+            printf("%d\t",*(arr+i));
+    }
+    else
+        printf("Chosen option is not valid\n");
+        exit(0);
     return 0;
 }
